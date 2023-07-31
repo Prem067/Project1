e37f14812bc6a598c576ada4f548096c768f273e
Prem067 <140488897+Prem067@users.noreply.github.com>
2023-07-31 17:01:48 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,6 @@
+function on(){
+    document.getElementById('img').src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Gluehlampe_01_KMJ.png/170px-Gluehlampe_01_KMJ.png";
+}
+function off(){
+    document.getElementById('img').src="https://cdn.mos.cms.futurecdn.net/HaPnm6P7TZhPQGGUxtDjAg-970-80.jpg";
+}
